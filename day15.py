9a9d4ec8f78661c37470696c8bc37fa35198ac8e
K. Lange <klange@toaruos.org>
2021-12-16 15:04:04 +0900

    more stuff

--- day15.py
+++ day15.py
@@ -11,73 +11,46 @@ lines = [line.strip() for line in data]
 
 print(len(lines))
 
-height = len(lines)
-width = len(lines[0])
+def runpart2(scale):
+    _height = len(lines)
+    _width = len(lines[0])
+    height = _height * scale
+    width = _width * scale
 
-def neighbors(coord):
-    y,x = coord
-    ds = [(-1,0),(1,0),(0,-1),(0,1)]
-    for dy,dx in ds:
-        if (x + dx) < 0: continue
-        if (x + dx) >= width * 5: continue
-        if (y + dy) < 0: continue
-        if (y + dy) >= height * 5: continue
-        yield (y+dy,x+dx)
+    nodes = [[0 for x in range(width)] for y in range(height)]
+    for y in range(_height):
+        for x in range(_width):
+            for _y in range(scale):
+                for _x in range(scale):
+                    nodes[y + _height * _y][x + _width * _x] = (int(lines[y][x]) + _y + _x + 8) % 9 + 1
 
-nodes = {}
-for y in range(height):
-    for x in range(width):
-        for _y in range(5):
-            for _x in range(5):
-                nodes[(y + height * _y, x + width * _x)] = (int(lines[y][x]) + _y + _x + 8) % 9 + 1
+    def neighbors(y,x):
+        ds = [(-1,0),(1,0),(0,-1),(0,1)]
+        for dy,dx in ds:
+            if (x + dx) < 0 or (x + dx) >= width or (y + dy) < 0 or (y + dy) >= height: continue
+            yield (y+dy,x+dx)
 
-for y in range(height*5):
-    for x in range(width*5):
-        print(nodes[(y,x)],end='')
-    print()
-
-def findMin(Q,dist):
-    found = None
-    for n in Q:
-        if found is None or dist[n] < dist[found]:
-            found = n
-    return found
-
-target = (height * 5 - 1, width * 5 - 1)
-
-import math
-
-def dijkstraThisShit(nodes, src):
-    Q = set()
-    dist = {}
-    prev = {}
-    for node in nodes.keys():
-        dist[node] = 10000000000000.0
-        prev[node] = None
-        Q.add(node)
-    dist[src] = 0
-    while Q:
-        u = findMin(Q,dist)
-        if u == target:
+    score = [[-1 for x in range(width)] for y in range(height)]
+    print("start")
+    score[0][0] = 0
+    generations = 0
+    while True:
+        changed = False
+        for y in range(height):
+            for x in range(width):
+                for _y,_x in neighbors(y,x):
+                    if score[_y][_x] == -1:
+                        continue
+                    cost = nodes[y][x] + score[_y][_x]
+                    if score[y][x] == -1 or cost < score[y][x]:
+                        score[y][x] = cost
+                        changed = True
+        generations += 1
+        if generations % 10 == 0:
+            print(generations,"generations")
+        if not changed:
             break
-        Q.remove(u)
-        for v in neighbors(u):
-            if v not in Q: continue
-            alt = dist[u] + nodes[v] # weight is on the node itself
-            if alt < dist[v]:
-                dist[v] = alt
-                prev[v] = u
-    return dist, prev
+    print(score[height-1][width-1])
+    print(generations,"generations total")
 
-dist, prev = dijkstraThisShit(nodes, (0,0))
-
-s = []
-u = target
-while u is not None:
-    s.append(u)
-    u = prev[u]
-
-print(nodes[(0,0)], nodes[target])
-print(sum(nodes[x] for x in s) - nodes[(0,0)])
-
-
+runpart2(5)
